0c7d43392472678243a9de476ba8adefdd63473c
Sixolise Klaas <sixoliseklaas17@gmail.com>
2021-07-01 02:49:04 -0700

    More pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,34 +8,34 @@
  */
 char *cap_string(char *str)
 {
-	int i;
+int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (i == 0)
-		{
-			if ((str[i] >= 97 && str[i] <= 122))
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
-		else if (str[i] == ' ')
-		{
-			++i;
-			if (str[i] >= 97 && str[i] <= 122)
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
-		else
-		{
-			if (str[i] >= 65 && str[i] <= 90)
-			{
-				str[i] = str[i] + 32;
-			}
-		}
-	}
-	return (str);
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i == 0)
+{
+if ((str[i] >= 97 && str[i] <= 122))
+{
+str[i] = str[i] - 32;
+continue;
+}
+}
+else if (str[i] == ' ')
+{
+++i;
+if (str[i] >= 97 && str[i] <= 122)
+{
+str[i] = str[i] - 32;
+continue;
+}
+}
+else
+{
+if (str[i] >= 65 && str[i] <= 90)
+{
+str[i] = str[i] + 32;
+}
+}
+}
+return (str);
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,30 +8,30 @@
  */
 char *leet(char *str)
 {
-	int i;
+int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == 'a' || str[i] == 'A')
-		{
-			str[i] = '4';
-		}
-		else if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-		}
-		else if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-		}
-		else if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-		}
-		else if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
-		}
-	}
-	return (str);
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] == 'a' || str[i] == 'A')
+{
+str[i] = '4';
+}
+else if (str[i] == 'e' || str[i] == 'E')
+{
+str[i] = '3';
+}
+else if (str[i] == 'o' || str[i] == 'O')
+{
+str[i] = '0';
+}
+else if (str[i] == 't' || str[i] == 'T')
+{
+str[i] = '7';
+}
+else if (str[i] == 'l' || str[i] == 'L')
+{
+str[i] = '1';
+}
+}
+return (str);
 }
