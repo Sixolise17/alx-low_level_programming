97324cc709f1766361135a6b5c36ee48a8c74df2
Sixolise Klaas <sixoliseklaas17@gmail.com>
2021-07-01 06:19:15 -0700

    More pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,30 @@
 #include "holberton.h"
-
 /**
- * *cap_string - capitalizes all words of a string.
- *
- * @str: hold character.
- * Return: string.
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
  */
-char *cap_string(char *str)
-{
-int i;
 
-for (i = 0; str[i] != '\0'; i++)
+char *cap_string(char *s)
 {
-if (i == 0)
-{
-if ((str[i] >= 97 && str[i] <= 122))
-{
-str[i] = str[i] - 32;
-continue;
-}
-}
-else if (str[i] == ' ')
-{
-++i;
-if (str[i] >= 97 && str[i] <= 122)
-{
-str[i] = str[i] - 32;
-continue;
-}
-}
-else
-{
-if (str[i] >= 65 && str[i] <= 90)
-{
-str[i] = str[i] + 32;
-}
-}
-}
-return (str);
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 13; i++)
+		{
+			if (*(s + count) == sep_words[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
+		}
+		count++;
+	}
+	return (s);
 }
