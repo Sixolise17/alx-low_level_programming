032829f12ffeeeb6accdcb740af47a967cb05b20
Sixolise Klaas <sixoliseklaas17@gmail.com>
2021-08-05 03:50:46 -0700

    BIT MANIPULATION

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,22 +10,22 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int v = 1;
-unsigned int i = 0;
-int c;
-unsigned int len;
+int i;
+unsigned int num;
 
-len = strlen(b);
-
-for (c = len - 1; c >= 0; c--)
-{
-if (b[c] != '0' && b[c] != '1')
+num = 0;
+if (!b)
 return (0);
-if (b[c] == '1')
+for (i = 0; b[i] != '\0'; i++)
 {
-i += v;
+if (b[i] != '0' && b[i] != '1')
+return (0);
 }
-v *= 2;
+for (i = 0; b[i] != '\0'; i++)
+{
+num <<= 1;
+if (b[i] == '1')
+num += 1;
 }
-return (i);
+return (num);
 }
